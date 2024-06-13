4a2f6acf3d98efa66451e31dccf868f30c169788
Stefanos <spalaio@moby-it.com>
2024-06-13 11:17:19 +0300

    seed champion improvement
    moved the bucketUrl variable to .env
    implemmented valibot to validate the schema of the heroes data.

--- seedChampions.ts
+++ seedChampions.ts
@@ -1,16 +1,17 @@
 import { getSupaBaseClient } from "./supabase.ts";
+import { load } from "https://deno.land/std@0.221.0/dotenv/mod.ts";
+import { validateInput } from "./utils.ts"
+const env = await load();
 const { data } = JSON.parse(await Deno.readTextFile('./champion.json'));
-
 const supabase = await getSupaBaseClient();
-const bucketUrl = 'https://znvtpipzflqwytxrtatb.supabase.co/storage/v1/object/public/champions/';
-const heroes = Object.entries(data).map(hero => hero[1]);
-
-const { error } = await supabase.from('champion').insert(heroes.map(
-  hero => {
-    if (hero && typeof hero === 'object' && 'name' in hero && 'id' in hero) {
-      return { name: hero.name, image_url: bucketUrl + hero.id + '_0.jpg' };
+const heroes = validateInput(Object.entries(data).map(hero => hero[1]));
+if (Array.isArray(heroes)) {
+    const { error } = await supabase.from('champion').insert(heroes.map(hero => {
+        if (hero && typeof hero === 'object' && 'name' in hero && 'id' in hero) {
+            return { name: hero.name, image_url: env["BUCKET_URL"] + hero.id + '_0.jpg' };
+        }
     }
-  }
-)
-);
-if (error) console.log(error.details);
+    )
+    );
+    if (error) console.log(error.details);
+}
--- utils.ts
+++ utils.ts
@@ -1,8 +1,25 @@
-export function getFiles(inputFolder:string):string[]{
-    const files :string[]= [];
-    const folder= Deno.readDirSync(inputFolder);
+import * as v from "https://deno.land/x/valibot@v0.31.1/mod.ts";
+
+const schema = v.object({
+    name: v.string(),
+    id: v.string(),
+});
+export function validateInput(input: unknown[]): unknown | null {
+    const validateRes = input.map(obje => {
+        const { success, output } = v.safeParse(schema, obje);
+        return { success, output }
+    })
+    if (validateRes.every(res => res.success === true)) {
+        return validateRes.map(res => res.output);
+    }
+    return null;
+}
+
+export function getFiles(inputFolder: string): string[] {
+    const files: string[] = [];
+    const folder = Deno.readDirSync(inputFolder);
     for (const file of folder) {
         files.push(file.name);
     }
-return files;
+    return files;
 }
